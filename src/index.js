d86b4bba943f102f6fb73bbc7fc92073653b3c87
Maksim Egorov <maks.egorov308@proton.me>
2018-09-09 16:58:59 +0300

    Added react-redux.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { createStore } from 'redux';
 import './index.css';
 import App from './App';
 import '../node_modules/font-awesome/css/font-awesome.min.css';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
+
+function friends(state = [], action) {
+    if (action.type === 'GET_FRIENDS') {
+        return [
+            ...state,
+            action.payload
+        ];
+    }
+    return state;
+}
+
+const store = createStore(friends);
+
+store.subscribe(() => {
+    console.log('subscribe', store.getState());
+});
+
+store.dispatch({type: 'GET_FRIENDS', payload: 'sdrfgsdf'});
+store.dispatch({type: 'GET_FRIENDS', payload: '12345sdf'});
 
 ReactDOM.render((
     <BrowserRouter>
